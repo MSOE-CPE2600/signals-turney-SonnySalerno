72ba5193463287e2a1944295a5496bf73bcf5258
SonnySalerno <sonny.salerno10@gmail.com>
2025-11-10 19:43:17 -0600

    updated .gitignore, added Makefile, updated signal handler and alarm

---
+++ Makefile
@@ -0,0 +1,17 @@
+CFLAGS=-c -g -Wall -Werror
+CC=gcc
+
+all: signal_handler signal_alarm
+
+signal_handler: signal_handler.o
+	$(CC) -o signal_handler signal_handler.o
+
+signal_alarm: signal_alarm.o
+	$(CC) -o signal_alarm signal_alarm.o
+
+clean:
+	rm -f *.o signal_handler signal_alarm
+
+%.o: %.c
+	$(CC) $(CFLAGS) -o $@ $<
+
--- signal_alarm.c
+++ signal_alarm.c
@@ -1,6 +1,27 @@
-/**
- * File: 
- * Modified by:
- * 
- * Brief summary of program:
- */
+/*******************************************************************
+* Filename: signal_alarm.c
+* Description: schedules a SIGALRM signal to be delivered to the 
+* process after 5 seconds.
+* Author: Sonny Salerno
+* Date: 11/10/2025
+* Note: make clean, make, ./signal_alarm 
+*********************************************************************/
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+void handle_alarm(int sig) {
+    printf("\nAlarm received!\n");
+}
+int main() {
+    signal(SIGALRM, handle_alarm);
+
+    printf("Alarm set for 5 seconds...\n");
+    alarm(5);
+
+    while (1) {
+        printf("Running...\n");
+        sleep(1);
+    }
+    return 0;
+}
