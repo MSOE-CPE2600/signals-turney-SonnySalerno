01a1e4d81b014dcb470644300461027160d063ed
SonnySalerno <sonny.salerno10@gmail.com>
2025-11-11 18:40:24 -0600

    updated .gitignore, Makefile, and added code for sigaction

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CFLAGS=-c -g -Wall -Werror
 CC=gcc
 
-all: signal_handler signal_alarm signal_segfault
+all: signal_handler signal_alarm signal_segfault signal_sigaction
 
 signal_handler: signal_handler.o
 	$(CC) -o signal_handler signal_handler.o
@@ -12,8 +12,11 @@ signal_alarm: signal_alarm.o
 signal_segfault: signal_segfault.o
 	$(CC) -o signal_segfault signal_segfault.o
 
+signal_sigaction: signal_sigaction.o
+	$(CC) -o signal_sigaction signal_sigaction.o
+
 clean:
-	rm -f *.o signal_handler signal_alarm signal_segfault
+	rm -f *.o signal_handler signal_alarm signal_segfault signal_sigaction
 
 %.o: %.c
 	$(CC) $(CFLAGS) -o $@ $<
--- signal_sigaction.c
+++ signal_sigaction.c
@@ -1,8 +1,39 @@
-/**
- * File: 
- * Modified by:
- * 
- * Brief summary of program:
- */
+/*******************************************************************
+* Filename: signal_sigaction.c
+* Description: 
+* Author: Sonny Salerno
+* Date: 11/10/2025
+* Note: make clean, make, ./signal_sigaction
+*********************************************************************/
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
 
+void handle_sigaction(int sig, siginfo_t *info, void *context) {
+    (void)context; 
+    printf("Received SIGUSR1 (signal %d)\n", sig);
+    printf("Signal sent by process with PID: %d\n", info->si_pid);
+}
 
+int main() {
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(sa));
+
+    sa.sa_sigaction = handle_sigaction;
+    sa.sa_flags = SA_SIGINFO;
+
+    if (sigaction(SIGUSR1, &sa, NULL) == -1) {
+        perror("sigaction");
+        return 1;
+    }
+
+    printf("Process running with PID: %d\n", getpid());
+    printf("Waiting for SIGUSR1 signal...\n");
+    printf("Send one using: kill -SIGUSR1 %d\n", getpid());
+
+    while (1) {
+        pause();
+    }
+    return 0;
+}
